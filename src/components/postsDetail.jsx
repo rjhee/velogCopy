760ceec2d3d9b7750a532ba238ff85011142e4bd
rjhee <qkdrkwogml@gmail.com>
2021-11-04 15:52:50 +0900

    📝 add comment form section

--- src/components/postsDetail.jsx
+++ src/components/postsDetail.jsx
@@ -2,19 +2,35 @@ import React from 'react';
 
 function PostsDetail() {
   return (
-    <section className="postsDetail-wrap">
-      <header>
-        <h1 className="postsDetail-title">title</h1>
-        <span className="postsDetail-user-name">user name</span>
-        <span className="postsDetail-created-date">create At</span>
-        <div className="postsDetail-tag-wrap">
-          <span>자바스크립트</span>
-          <span>웹개발</span>
-          <span>프론트엔드</span>
-        </div>
-      </header>
-      <div className="postsDetail-content">본문내용</div>
-    </section>
+    <div className="postsDetail-wrap">
+      <section className="postsDetail">
+        <header>
+          <h1 className="postsDetail-title">title</h1>
+          <span className="postsDetail-user-name">user name</span>
+          <span className="postsDetail-created-date">create At</span>
+          <div className="postsDetail-tag-wrap">
+            <span>자바스크립트</span>
+            <span>웹개발</span>
+            <span>프론트엔드</span>
+          </div>
+        </header>
+        <div className="postsDetail-content">본문내용</div>
+      </section>
+      <section className="postsDetail-comment">
+        <h2 className="postsDetail-comment-title">댓글</h2>
+        <form>
+          <input
+            name="comment"
+            className="postsDetail-comment-input"
+            type="text"
+            placeholder="댓글을 입력하세요"
+          />
+          <button className="postsDetail-comment-button" type="submit">
+            댓글 작성
+          </button>
+        </form>
+      </section>
+    </div>
   );
 }
 
