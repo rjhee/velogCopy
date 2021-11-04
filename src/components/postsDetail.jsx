c18ea536c59988c1a90e1c545739d7d1b9ea1706
rjhee <qkdrkwogml@gmail.com>
2021-11-04 16:24:30 +0900

    📝 add wrap tag to header in postsDeatil section

--- src/components/postsDetail.jsx
+++ src/components/postsDetail.jsx
@@ -5,21 +5,25 @@ function PostsDetail() {
     <div className="postsDetail-wrap">
       <section className="postsDetail">
         <header>
-          <h1 className="postsDetail-title">title</h1>
-          <span className="postsDetail-user-name">user name</span>
-          <span className="postsDetail-created-date">create At</span>
+          <h1 className="postsDetail-title title">title</h1>
+          <div className="postsDetail-info-wrap">
+            <span className="postsDetail-user-name">user name</span>
+            <span className="postsDetail-created-date">2021.01.01</span>
+          </div>
           <div className="postsDetail-tag-wrap">
             <span>자바스크립트</span>
             <span>웹개발</span>
             <span>프론트엔드</span>
           </div>
         </header>
-        <div className="postsDetail-content">본문내용</div>
+        <div className="postsDetail-content">
+          본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용본문내용
+        </div>
       </section>
       <section className="postsDetail-comment">
-        <h2 className="postsDetail-comment-title">댓글</h2>
+        <h2 className="postsDetail-comment-title title">댓글</h2>
         <form>
-          <input
+          <textarea
             name="comment"
             className="postsDetail-comment-input"
             type="text"
