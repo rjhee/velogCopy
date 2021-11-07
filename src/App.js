39fe50558de7de14c0c908caffd8bd7f4f1c77eb
rjhee <qkdrkwogml@gmail.com>
2021-11-07 21:31:35 +0900

    ✨ create Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './styles/App.css';
+
 import PostsDetail from './components/postsDetail';
-import Upload from './components/Upload.1';
+import Upload from './components/upload';
+import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
+
 import '@fortawesome/fontawesome-free/js/all.js';
 
-function App() {
+const App = () => {
   return (
     <>
-      <Upload></Upload>
-      <PostsDetail></PostsDetail>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/Upload" component={Upload}></Route>
+          <Route path="/postsDetail" component={PostsDetail}></Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
