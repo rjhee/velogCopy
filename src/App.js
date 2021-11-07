cf9aa3cf67f6adf51407f8fa42e2da90fc6e7c11
rjhee <qkdrkwogml@gmail.com>
2021-11-07 21:59:21 +0900

    add Route

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import './styles/App.css';
 
-import PostsDetail from './components/postsDetail';
-<<<<<<< HEAD
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PostsDetail from './components/postsDetail';
 import Listpage from './pages/Listpage';
-=======
 import Upload from './components/upload';
-import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
 
 import '@fortawesome/fontawesome-free/js/all.js';
->>>>>>> 39fe50558de7de14c0c908caffd8bd7f4f1c77eb
 
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
-    <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<Listpage />} />
-        <Route path="detail" element={<PostsDetail />}  />
-      </Routes>
-    </BrowserRouter>
-=======
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/Upload" component={Upload}></Route>
-          <Route path="/postsDetail" component={PostsDetail}></Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Listpage />} />
+          <Route path="detail" element={<PostsDetail />} />
+          <Route exact path="/Upload" element={<Upload />} />
+        </Routes>
       </BrowserRouter>
->>>>>>> 39fe50558de7de14c0c908caffd8bd7f4f1c77eb
     </>
   );
 };
